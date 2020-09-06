444c9c4d91d15c37ab75756ef4d908003f5375fb
suhongkim98 <kshz9815@naver.com>
2020-09-07 01:01:19 +0900

    init: setting styled component, responsive

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import GlobalStyle from './styles/GlobalStyles';
+import { ThemeProvider } from './styles/theme-components';
+import theme from './styles/theme';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+        <>
+        <GlobalStyle/>  
+        <ThemeProvider theme={theme}>
+            <App />
+        </ThemeProvider>
+        </>
+        , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
