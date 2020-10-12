d20707b9bc90078e73089760a2bfaf6616bb4bb3
suhongkim98 <kshz9815@naver.com>
2020-10-12 21:13:43 +0900

    Update: MainPage

--- src/router/MainPage.tsx
+++ src/router/MainPage.tsx
@@ -1,5 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Redirect } from 'react-router-dom';
 import styled from 'styled-components';
+import FloatDiv from '../components/FloatDiv';
+import LogoutSVG from '../images/LogoutSVG';
+import SettingSVG from '../images/SettingSVG';
+import { StrongboxDatabase } from '../StrongboxDatabase';
 import theme from '../styles/theme';
 
 interface SpanProps {
@@ -44,6 +49,19 @@ background-color:${theme.colors.headerBackgroundColor};
 border-style:solid;
 border-width:1px;
 border-color:gray;
+
+display: flex;
+justify-content: space-between;
+align-items: center;
+
+padding-left: 25px;
+padding-right: 25px;
+`;
+
+const NameHeaderInnerWrapper = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
 `;
 const NavBarWrapper = styled.div`
 width:100%;
@@ -59,6 +77,15 @@ height:100%;
 border-style:solid;
 border-width:1px;
 border-color:gray;
+
+display: flex;
+justify-content: center;
+align-items: center;
+`;
+
+const FloatDivWrapper = styled.div`
+width:97%;
+height:97%;
 `;
 
 const AddFolderBtn = styled.button`
@@ -76,12 +103,33 @@ padding:5px 50px;
 
 // global idx = 대상 유저 idx, global key = 대칭키 암호키
 const MainPage:React.FC = () =>{
+    const [redirect, setRedirect] = useState("");
+    const [name,setName] = useState("null");
 
+    useEffect(() =>{
+        const database = StrongboxDatabase.getInstance();
+        database.select("NAME","USERS_TB","IDX = " + global.idx).then((result:any) =>{
+            setName(result[0].NAME);
+        }).catch((error)=>{
+            console.log(error);
+        });
+    },[]);
+
+    const onLogoutButtonClicked = () =>{
+        global.idx = -1;
+        global.key = "";
+        setRedirect("/");
+    }
+    const onSettingButtonClicked = () =>{
+
+    }
+
+    if(redirect !== "") return <Redirect to={redirect}/>
     return <TotalWrapper>
         <SearchHeaderWrapper></SearchHeaderWrapper>
-        <NameHeaderWrapper></NameHeaderWrapper>
+        <NameHeaderWrapper><NameHeaderInnerWrapper><Span textColor="white" size="3rem">{name}</Span><div onClick={onLogoutButtonClicked}><LogoutSVG width="30px" height="30px" color="white"/></div></NameHeaderInnerWrapper><NameHeaderInnerWrapper><div onClick={onSettingButtonClicked}><SettingSVG width="30px" height="30px" color="white"/></div></NameHeaderInnerWrapper></NameHeaderWrapper>
         <NavBarWrapper><AddFolderBtn><Span textColor="white" size="1.6rem">폴더 추가하기</Span></AddFolderBtn></NavBarWrapper>
-        <MainWrapper></MainWrapper>
+        <MainWrapper><FloatDivWrapper><FloatDiv title="test" /></FloatDivWrapper></MainWrapper>
     </TotalWrapper>
 }
 
