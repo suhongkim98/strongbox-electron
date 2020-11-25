de51c7a793a8158466accbdfbdbf8d27dadcb928
suhongkim98 <kshz9815@naver.com>
2020-11-25 22:21:39 +0900

    계정 추가버튼 추가

--- src/components/AccountView.tsx
+++ src/components/AccountView.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import PlusSVG from '../images/PlusSVG';
 
 const TotalWrapper = styled.div`
 width: 100%;
@@ -7,7 +8,8 @@ height: 100%;
 padding: 25px 60px 0 60px;
 `;
 
-const AccountList = styled.div`
+
+const InnerWrapper = styled.div`
 width:100%;
 height: 100%;
 
@@ -22,8 +24,23 @@ overflow-y:scroll;
     display:none;
 }
 `;
+const ViewCard = styled.div`
+width:100%;
+height:100%;
+background-color:white;
+
+border-style:solid;
+border-width:1px;
+border-color:black;
+`;
+const AddAccountBtn = styled(ViewCard)`
+display:flex;
+justify-content:center;
+align-items:center;
+`;
 const AccountView = () => {
-    return <TotalWrapper><AccountList><Account/><Account/><Account/><Account/><Account/><Account/><Account/><Account/><Account/><Account/><Account/><Account/><Account/><Account/></AccountList></TotalWrapper>
+    return <TotalWrapper><InnerWrapper><Account/><Account/><Account/><Account/><Account/><Account/><Account/><Account/><Account/><Account/><Account/><Account/><Account/><Account/>
+    <AddAccountBtn><PlusSVG width="50px" height="50px" color="gray" /></AddAccountBtn></InnerWrapper></TotalWrapper>
 }
 
 export default AccountView;
@@ -36,16 +53,7 @@ interface AccountProps{
     accountID: string;
     accountPassword: string;
 }
-const Wrapper = styled.div`
-width:100%;
-height:100%;
-background-color:white;
-
-border-style:solid;
-border-width:1px;
-border-color:black;
-`;
 
 const Account = () => {
-    return <Wrapper></Wrapper>
+    return <ViewCard></ViewCard>
 }
